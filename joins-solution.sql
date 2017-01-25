e8e104a5b38c5f2c80bb3647ec0f23c0145a45e1
Noonsky <noonan135@gmail.com>
2017-01-25 16:40:04 -0600

    Challenge completed

---
+++ joins-solution.sql
@@ -0,0 +1,36 @@
+--1 Get all customers and their addresses.
+SELECT * FROM customers
+JOIN addresses ON customers.id = addresses.customer_id;
+
+--2 Get all orders and their line items.
+SELECT * FROM orders
+JOIN line_items ON orders.id = line_items.order_id;
+
+--3 Which warehouses have cheetos?
+SELECT warehouse FROM products
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON  warehouse_product.warehouse_id = warehouse.id
+WHERE description = 'cheetos';
+
+--4 Which warehouses have diet pepsi?
+SELECT warehouse FROM products
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON  warehouse_product.warehouse_id = warehouse.id
+WHERE description = 'diet pepsi';
+
+--5 Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT first_name, last_name, COUNT(address_id)  FROM customers
+JOIN addresses ON customers.id = addresses.customer_id
+JOIN orders ON addresses.id = orders.address_id
+GROUP BY customers.id;
+
+--6 How many customers do we have?
+SELECT COUNT(id)  FROM customers;
+
+--7 How many products do we carry?
+SELECT COUNT(description) FROM products;
+
+--8 What is the total available on-hand quantity of diet pepsi?
+SELECT SUM(on_hand) FROM warehouse_product
+JOIN products ON warehouse_product.product_id = products.id
+WHERE description = 'diet pepsi';
